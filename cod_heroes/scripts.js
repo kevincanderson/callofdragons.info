ab18d2fd4d48062615096eadbfe490b0088a1322
Anderson, Kevin <kevin.anderson@intelsat.com>
2025-04-07 15:50:12 -0500

    added more heroes, still need images

--- cod_heroes/scripts.js
+++ cod_heroes/scripts.js
@@ -38,11 +38,11 @@ fetch('heroes.json')
     console.log(heroesDataArr);
     heroesDataArr.forEach(hero => {
         heroesContainer.innerHTML += `
-        <div class="hero-card">
+        <div href="" class="hero-card">
         <div class="hero-info-container">
         <div class="hero-info-top">
         <p class="hero-title">${hero.heroTitle}</p>
-          <h2 class="hero-name">${hero.name}</h2>
+          <a href="./heroguide/${hero.name}" class="hero-link"><h2 class="hero-name">${hero.name}</h2></a>
         <p class="hero-specialization">${hero.specializations.specializationOne} | ${hero.specializations.specializationTwo} | ${hero.specializations.specializationThree}</p>
         </div>
         <div class="hero-info-bottom">
